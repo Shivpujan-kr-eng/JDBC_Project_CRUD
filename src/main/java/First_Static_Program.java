c33d17a40273023247af6d55f4e87b2fac07694f
shivpujan kumar <sahil.8228053857@gmail.com>
2023-02-16 00:17:18 +0530

    First Commit......

---
+++ src/main/java/First_Static_Program.java
@@ -0,0 +1,9 @@
+
+public class First_Static_Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("be happy , i am going to make project which will cover approximately all crud commands required in jdbc");
+	}
+
+}
